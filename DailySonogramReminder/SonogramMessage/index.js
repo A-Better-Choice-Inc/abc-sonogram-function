ef7f9e686410031deccfee77d5ebfc865f3e5927
Ryan Dick <rdick@pregnancywichita.com>
2025-10-08 21:08:26 -0500

    Update to use PHP API endpoint for sonogram reminders

--- DailySonogramReminder/SonogramMessage/index.js
+++ DailySonogramReminder/SonogramMessage/index.js
@@ -4,7 +4,8 @@ const { DateTime } = require("luxon");
 
 console.log("Begin Script")
 
-let sourceUrl = `https://abc-sonogram-api.azurewebsites.net/sonograms/GetTomorrowSonosOKToText`
+// Update to use the new PHP API endpoint
+let sourceUrl = `https://abcmc.azurewebsites.net/api/v1/sonogram_reminders.php`
 let twillioFlowId = process.env["twillioFlowId"]
 let twilioAccountSid = process.env["twilioAccountSid"]
 let twilioAuthToken = process.env["twilioAuthToken"]
@@ -12,36 +13,39 @@ let twilioAuthToken = process.env["twilioAuthToken"]
 module.exports = async function (context, myTimer) {
     const client = twillio(twilioAccountSid, twilioAuthToken);
     console.log("Fetch Data From Source URL")
-    
-    let responseData  = await axios.get(sourceUrl)
-    // console.log("ResponseData: ", responseData)
-    
-    responseData.data.data.forEach(async line => {
-        console.log("Response Date:", line.date)
-        console.log("Response Time:", line.time)
 
-        fulldate = DateTime.fromISO(line.date).setZone("utc")
+    let responseData = await axios.get(sourceUrl)
+
+    responseData.data.data.forEach(async line => {
+        // Destructure the array returned by stored procedure
+        // [id, client_id_fk, date, time, firstName, lastName, Phone, okToText]
+        const [id, client_id_fk, date, time, firstName, lastName, Phone, okToText] = line;
+
+        console.log("Response Date:", date)
+        console.log("Response Time:", time)
+
+        fulldate = DateTime.fromISO(date).setZone("utc")
         console.log("Luxon: ", fulldate)
-        
+
         monthnumber = fulldate.toFormat('LL')
         daynumber = fulldate.toFormat('dd')
         dayofweek = fulldate.toFormat('cccc')
-        
-        fulltime = DateTime.fromISO(line.time).setZone("America/Chicago")
+
+        fulltime = DateTime.fromISO(time).setZone("America/Chicago")
         console.log("Luxon: ", fulltime)
         combinedTime = fulltime.toFormat('hh:mm a')
 
         let twillioParams = {
-            to: line.Phone, 
+            to: Phone,
             from: '+13166855757',
             parameters: {
-                sonogram_id: line.id,
+                sonogram_id: id,
                 textmessage: [
                     "This is a reminder for your appointment at A BETTER CHOICE on",
                     `${dayofweek}, ${monthnumber}/${daynumber} at ${combinedTime}. Please`,
                     "reply Y to confirm and N to cancel. Thanks!"
                 ].join(" "),
-                client_id: line.client_id_fk,
+                client_id: client_id_fk,
                 type: 'sonogram',
             }
         }
